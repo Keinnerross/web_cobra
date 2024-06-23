e4ae9f6a978c9f19b0c9704d7c7051b7afe182ac
keinnerross <keinnerross@gmail.com>
2024-06-23 15:15:01 -0400

    update

--- src/app/components/mainSlider.jsx
+++ src/app/components/mainSlider.jsx
@@ -4,10 +4,10 @@ const MainSlider = ({ title, textButton }) => {
             backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.23301820728291311) 0%, rgba(8,8,8,0.8016456582633054) 70%), url('/assets/bg.png')`,
         }}>
             <div className="flex flex-col items-center py-[160px] h-full gap-8">
-                <span className="text-white text-[50px] xl:text-[72px] font-black xl:w-[50%] text-center leading-tight	">{title}</span>
+                <span className="text-white text-[50px] xl:text-[72px] font-black w-[90%] xl:w-[50%] text-center leading-tight	">{title}</span>
 
 
-                <a href="#modelos"><button className=" text-white text-[22px] font-bold rounded-full py-4 px-8 bg-red-800 hover:bg-red-600 cursor-pointer">{textButton}</button></a>
+                <a href="#modelos" className="flex justify-center "><button className="w-[90%] text-white text-[22px] font-bold rounded-full py-4 px-8 bg-red-800 hover:bg-red-600 cursor-pointer">{textButton}</button></a>
             </div>
         </div>
     )
